29a9594f3fd46744eb161b8a59bd9f83debb6d83
朱骏杰 <zhujunjie@sundear.com>
2021-05-11 09:18:43 +0800

    加上loading

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
       <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-      <g-icon class="loading" :name="'right'"></g-icon>
+      <g-icon class="loading" :name="'loading'"></g-icon>
 
       <div class="content">
          <slot></slot>
@@ -70,8 +70,8 @@
             order: 2
          }
       }
-      &.loading {
-         animation: s 1s infinite linear;
+      .loading {
+         animation: spin 2s infinite linear;
       }
    }
 </style>
