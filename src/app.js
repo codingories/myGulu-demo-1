d8b82b4b7ebb05aebcf04d238db53d9f78652822
朱骏杰 <zhujunjie@sundear.com>
2021-05-13 21:09:38 +0800

    使用 chai.expect 完成四个测试用例 接下去做第五个函数的测试

--- src/app.js
+++ src/app.js
@@ -21,27 +21,101 @@ const expect = chai.expect
 
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData: {
       icon: 'settings'
     }
   })
-  button.$mount('')
-  let useElement = button.$el.querySelector('use')
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
   let href = useElement.getAttribute('xlink:href')
   expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+
 }
 
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData: {
       icon: 'settings',
       loading: true
     }
   })
-  button.$mount('')
-  let useElement = button.$el.querySelector('use')
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
   let href = useElement.getAttribute('xlink:href')
   expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
 }
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  // 计算后的1
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition:'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function(){
+    // 新手的写法，实际上是错误的
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
