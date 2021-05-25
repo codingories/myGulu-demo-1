b0a9bc1fc4e947381336ece8e7e3d6656b11d8e5
朱骏杰 <zhujunjie@sundear.com>
2021-05-25 08:43:00 +0800

    开始做1和11分

---
+++ src/col.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col">
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+  .col {
+    height: 100px;
+    background-color: blue;
+    width: 50%;
+    border: 1px solid red;
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
---
+++ temp.js
@@ -0,0 +1,5 @@
+@for $n from 1 through 24 {
+&.#{$class}#{$n} {
+    width: {$n / 24} * 100%;
+  }
+}
