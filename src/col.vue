944d9a0713f4f67c94e64b040d41f1d3cf3042ba
朱骏杰 <zhujunjie@sundear.com>
2021-05-28 19:36:06 +0800

    默认就是phone手机样式

--- src/col.vue
+++ src/col.vue
@@ -27,7 +27,6 @@
       gutter: {
         type: [Number, String]
       },
-      phone: {type: Object, validator},
       ipad: {type: Object, validator},
       narrowPc: {type: Object, validator},
       pc: {type: Object, validator},
@@ -42,15 +41,14 @@
     },
     computed: {
       colClass(){
-        let { span, offset, phone, ipad, narrowPc, pc, widePc } = this
+        let { span, offset, ipad, narrowPc, pc, widePc } = this
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ...[phone && `col-phone-${phone.span}`],
           ...[ipad && `col-ipad-${ipad.span}`],
-          ...[narrowPc && `col-narrowPc-${narrowPc.span}`],
+          ...[narrowPc && `col-narrow-pc-${narrowPc.span}`],
           ...[pc && `col-pc-${pc.span}`],
-          ...[widePc && `col-widePc-${widePc.span}`],
+          ...[widePc && `col-wide-pc-${widePc.span}`],
         ]
       },
       colStyle(){
@@ -78,20 +76,7 @@
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
+
     @media (min-width: 577px) and (max-width: 768px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
