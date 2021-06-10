b72ab62ed3d498365842c8d0950c9b9d94bd1674
朱骏杰 <zhujunjie@sundear.com>
2021-06-10 19:37:54 +0800

    完成toast 测试用例

--- src/app.js
+++ src/app.js
@@ -59,8 +59,7 @@ new Vue({
             console.log('他说已经充值智商了')
           }
         },
-        autoClose:  false,
-        autoCloseDelay: 3
+        autoClose:  3,
       })
     }
   }
--- src/toast.vue
+++ src/toast.vue
@@ -18,12 +18,11 @@
     name: 'GuluToast',
     props: {
       autoClose: {
-        type: Boolean,
-        default: true
-      },
-      autoCloseDelay: {
-        type: Number,
-        default: 50
+        type: [ Boolean, Number],
+        default: 5,
+        validator(value) {
+          return value === false || typeof value === 'number';
+        }
       },
       closeButton: {
         type: Object,
@@ -60,22 +59,21 @@
         if (this.autoClose) {
           setTimeout( ()=>{
             this.close()
-          } , this.autoCloseDelay * 1000)
+          } , this.autoClose * 1000)
         }
       },
       updateStyles(){
         this.$nextTick(() => {
-          this.$refs.line.style.height =
-            `${this.$refs.toast.getBoundingClientRect().height}px`
+          if(this.$refs.line) {
+            this.$refs.line.style.height =
+              `${this.$refs.toast.getBoundingClientRect().height}px`
+          }
         })
       },
       close () {
         this.$el.remove()
         this.$emit('close')
         this.$destroy()
-      },
-      log () {
-        console.log('测试')
       },
       onClickClose () {
         this.close()
