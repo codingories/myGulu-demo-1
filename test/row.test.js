381f13d65f49036ab52e1c590d24dcaf95ab7fa0
朱骏杰 <zhujunjie@sundear.com>
2021-06-11 09:41:49 +0800

    解决ci上linux引入文件大小写问题

--- test/row.test.js
+++ test/row.test.js
@@ -1,7 +1,7 @@
 const expect = chai.expect;
 import Vue from 'vue'
-import Row from '../src/Row'
-import Col from '../src/Col'
+import Row from '../src/row'
+import Col from '../src/col'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
