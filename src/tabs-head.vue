5e880e7c2a2ad09e2ecbfb7cc70f34a220af5b0c
朱骏杰 <zhujunjie@sundear.com>
2021-06-11 15:52:58 +0800

    添加 tabs 相关组件， 添加基本 props

---
+++ src/tabs-head.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tabs-head">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GuluHead'
+  }
+</script>
+<style>
+.tabs-head {
+
+}
+</style>
+
---
+++ src/tabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tabs-tabs">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'GuluTabsTabs',
+    props: {
+      selected: {
+        type: String,
+        required: true
+      },
+      direction: {
+        type: String,
+        default: 'horizontal',
+        validator (value) {
+          return ['horizontal', 'vertical'].indexOf(value) >= 0
+        }
+      }
+    },
+    created(){
+      // 这个组件必须要触发update:selected事件才能使外面.sync有用
+      // this.$emit('update:selected', 'xxx')
+    }
+  }
+</script>
+<style>
+  .tabs-tabs {
+
+  }
+</style>
+
