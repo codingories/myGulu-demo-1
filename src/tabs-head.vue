21ecee90149766a578df36f7779572f370a45bb4
朱骏杰 <zhujunjie@sundear.com>
2021-06-15 11:49:14 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="action-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -10,15 +11,27 @@
   export default {
     name: 'GuluTabsHead',
     inject: ['eventBus'],
-    created(){}
+    created(){
+      this.eventBus.$on('update:selected', (item, vm) => {
+        console.log(item, vm)
+      })
+    }
   }
 </script>
 <style scoped lang="scss">
   $tab-height: 40px;
+  $blue: blue;
   .tabs-head {
     display: flex;
     height: $tab-height;
     border: 1px solid red;
+    position: relative;
+    > .line {
+      position: absolute;
+      bottom: 0;
+      border-bottom:1px solid $blue;
+      width: 100px;
+    }
     > .actions-wrapper {
       margin-left: auto;
     }
--- src/tabs.vue
+++ src/tabs.vue
@@ -31,11 +31,17 @@
       }
     },
     created(){
-      // this.$emit('update:selected', '这是 this $emit 出来的数据')
-      // this.eventBus.$emit('update:selected', '这是 this eventBus $emit 出来的数据')
     },
     mounted() {
-      this.eventBus.$emit('update:selected', this.selected)
+      this.$children.forEach(vm=>{
+        if (vm.$options.name === 'GuluTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
     }
   }
 </script>
