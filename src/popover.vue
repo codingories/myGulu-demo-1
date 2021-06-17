6e20880877dd5e79b1489f0359f5e200e05df3ef
朱骏杰 <zhujunjie@sundear.com>
2021-06-17 10:27:35 +0800

    增加点击popover里面内容的判断

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper"  v-show="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper"  v-show="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
     </span>
   </div>
@@ -21,26 +21,33 @@
     methods: {
       positionContent() {
         document.body.appendChild(this.$refs.contentWrapper)
-        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        let { top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
         this.$refs.contentWrapper.style.top = top + window.scrollY+ 'px'
       },
       onClickDocument (e) {
+        // div已经移到popover外面去了，当用户点击popover的时候还是点击Popover外面
         if (this.$refs.popover &&
           (this.$refs.popover === e.target ||
-            this.$refs.popover.contains(e.target))) { return }
+            this.$refs.popover.contains(e.target))
+        ) {return }
+        // 再加个判断，如果this.$refs有contentWrapper,而且点的东西就是contentWrapper,或者点的东西在contentWrapper里面就return
+        if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target ||
+            this.$refs.contentWrapper.contains(e.target))
+        ) { return }
         this.close()
       },
       open (){
         setTimeout(()=>{
           this.visible = true
           this.positionContent()
-          document.addEventListener('click', this.eventHandler)
+          document.addEventListener('click', this.onClickDocument)
         })
       },
       close() {
         this.visible = false
-        document.removeEventListener('click', this.eventHandler)
+        document.removeEventListener('click', this.onClickDocument)
       },
       onClick (event) {
         if (this.$refs.triggerWrapper.contains(event.target)) {
@@ -55,15 +62,40 @@
 }
 </script>
 <style lang='scss' scoped>
-  .popover { 
+  $border-color: #333;
+  $border-radius: 4px;
+  .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
   }
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
     box-shadow: 0 0 3px rgb(0,0,0, 0.5);
     transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      top: 100%;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
 </style>
