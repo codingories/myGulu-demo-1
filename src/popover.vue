a3e8a45fad38c9d0f9a16df754ae080fddd3bb51
朱骏杰 <zhujunjie@sundear.com>
2021-06-22 08:33:24 +0800

    使用表驱动的方式进行重构代码

--- src/popover.vue
+++ src/popover.vue
@@ -28,25 +28,16 @@
       positionContent() {
         const { contentWrapper, triggerWrapper } = this.$refs
         document.body.appendChild(contentWrapper)
-        let { width, height, top, left} = triggerWrapper.getBoundingClientRect()
-        if (this.position === 'top') {
-          contentWrapper.style.left = left + window.scrollX + 'px'
-          contentWrapper.style.top = top + window.scrollY+ 'px'
-        } else if (this.position === 'bottom') {
-          contentWrapper.style.left = left + window.scrollX + 'px'
-          contentWrapper.style.top = top + height + window.scrollY+ 'px'
-        } else if (this.position === 'left') {
-          contentWrapper.style.left = left + window.scrollX + 'px'
-          let { height: height2 } = contentWrapper.getBoundingClientRect()
-          contentWrapper.style.top = top + window.scrollY +
-            (height - height2 ) / 2 + 'px'
-        } else if (this.position === 'right') {
-          contentWrapper.style.left = left + window.scrollX + width + 'px'
-          contentWrapper.style.top = top + window.scrollY+ 'px'
-          let { height: height2 } = contentWrapper.getBoundingClientRect()
-          contentWrapper.style.top = top + window.scrollY +
-            (height - height2 ) / 2 + 'px'
+        const { width, height, top, left} = triggerWrapper.getBoundingClientRect()
+        const { height: height2 } = contentWrapper.getBoundingClientRect()
+        let positions = {
+          top: {top: top + window.scrollY, left: left + window.scrollX},
+          bottom: {top: top + height + window.scrollY, left: left + window.scrollX},
+          left: {top: top + window.scrollY + (height - height2 ) / 2, left: left + window.scrollX},
+          right: {top: top + window.scrollY + (height - height2 ) / 2, left: left + window.scrollX + width}
         }
+        contentWrapper.style.left = positions[this.position].left + 'px'
+        contentWrapper.style.top = positions[this.position].top + 'px'
       },
       onClickDocument (e) {
         if (this.$refs.popover &&
@@ -63,12 +54,14 @@
         this.visible = true
         this.$nextTick(() => {
           this.positionContent()
+          console.log('open')
           document.addEventListener('click', this.onClickDocument)
         })
       },
       close() {
         this.visible = false
         document.removeEventListener('click', this.onClickDocument)
+        console.log('close')
       },
       onClick (event) {
         if (this.$refs.triggerWrapper.contains(event.target)) {
