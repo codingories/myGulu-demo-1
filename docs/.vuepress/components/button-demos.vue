26a24864b675d4f2768c53d6fd90952a1d639080
朱骏杰 <zhujunjie@sundear.com>
2021-06-25 19:12:59 +0800

    创建观望

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <g-button>默认按钮</g-button>
+    <g-button icon=""settings>默认按钮</g-button>
+    <g-button :loading="true">默认按钮</g-button>
+    <g-button disabled>默认按钮</g-button>
+    <pre>
+      <code>
+        {{content}}
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+  import Button from '../../../src/button.vue'
+
+  export default {
+    data() {
+      return {
+        content: `
+        <g-button>默认按钮</g-button>
+        <g-button icon=""settings>默认按钮</g-button>
+        <g-button :loading="true">默认按钮</g-button>
+        <g-button disabled>默认按钮</g-button>
+        `
+      }
+    },
+    components: {
+      'g-button': Button,
+
+    }
+  }
+</script>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: '轱辘UI',
+  description: '一个好用UI页面',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        children: ['/components/button']
+      },
+    ]
+  }
+}
