1061135a6c679a512e2ac76da6454a8467d78225
朱骏杰 <zhujunjie@sundear.com>
2021-08-16 07:53:34 +0800

    90开始重构

---
+++ src/validate.js
@@ -0,0 +1,39 @@
+export default function validate(data, rules) {
+  let errors = {}
+  rules.forEach((rule)=> {
+    let value = data[rule.key]
+    if(rule.required) {
+      if(value !==0 && !value) {
+        errors[rule.key] = {required: '必填'}
+        return
+      }
+    }
+    if (rule.pattern) {
+      if(rule.pattern === 'email') {
+        rule.pattern = /^.+@.+$/
+      }
+      if (rule.pattern.test(value) === false) {
+        ensureObject(errors, rule.key)
+         errors[rule.key].pattern = '格式不正确'
+        console.log(errors)
+      }
+    }
+    if (rule.minLength) {
+      if (value.length < rule.minLength) {
+
+        ensureObject(errors, rule.key)
+        errors[rule.key].minLength= '太短'
+
+        console.log(errors)
+      }
+    }
+
+  })
+  return errors
+}
+
+function ensureObject(obj, key) {
+  if(typeof obj[key] !== 'object') {
+    obj[key] = {}
+  }
+}
