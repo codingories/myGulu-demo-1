fe425556f09f19717555fbdc7826dacb5a577165
朱骏杰 <zhujunjie@sundear.com>
2021-08-17 14:17:08 +0800

    90 js 本地验证完成

--- src/validate.js
+++ src/validate.js
@@ -3,33 +3,57 @@ export default function validate(data, rules) {
   rules.forEach((rule)=> {
     let value = data[rule.key]
     if(rule.required) {
-      if(value !==0 && !value) {
-        errors[rule.key] = {required: '必填'}
+      let error = validate.required(value)
+      if(error) {
+        ensureObject(errors, rule.key)
+        errors[rule.key].required = error
         return
       }
     }
-    if (rule.pattern) {
-      if(rule.pattern === 'email') {
-        rule.pattern = /^.+@.+$/
-      }
-      if (rule.pattern.test(value) === false) {
-        ensureObject(errors, rule.key)
-         errors[rule.key].pattern = '格式不正确'
-        console.log(errors)
-      }
-    }
-    if (rule.minLength) {
-      if (value.length < rule.minLength) {
 
-        ensureObject(errors, rule.key)
-        errors[rule.key].minLength= '太短'
-
-        console.log(errors)
+    let validators = Object.keys(rule).filter(key => (key !== 'key') && (key !== 'required'))
+    // 遍历 vali dators, 并逐一调用对应的函数
+    validators.forEach((validatorKey) => {
+      // key is pattern / minLength / maxLength / hasNumber string
+      if(validate[validatorKey]) {
+        let error = validate[validatorKey](value, rule[validatorKey])
+        if (error) {
+          ensureObject(errors, rule.key)
+          errors[rule.key][validatorKey] = error
+        }
+      } else {
+        throw `不存在的校验器: ${validatorKey}`
       }
-    }
-
+    })
   })
   return errors
+}
+
+validate.required = (value) => {
+  if(value !==0 && !value) {
+    return '必填'
+  }
+}
+
+validate.pattern = (value, pattern) => {
+  if(pattern === 'email') {
+    pattern = /^.+@.+$/
+  }
+  if (pattern.test(value) === false) {
+    return '格式不正确'
+  }
+}
+
+validate.minLength = (value, minLength) => {
+  if (value.length < minLength) {
+    return '太短'
+  }
+}
+
+validate.maxLength = (value, maxLength) => {
+  if (value.length > maxLength) {
+    return '太长'
+  }
 }
 
 function ensureObject(obj, key) {
--- tests/unit/validate.spec.js
+++ tests/unit/validate.spec.js
@@ -96,4 +96,80 @@ describe('validate', () => {
     expect(errors.email.minLength).to.exist
     expect(errors.email.pattern).to.exist
   })
+
+  it('maxLength', () => {
+    let data = {
+      email: '123123123123'
+    }
+    let rules = [
+      {key: 'email', pattern: 'email', maxLength: 10}
+    ]
+    let errors = validate(data, rules)
+    expect(errors.email.maxLength).to.exist
+  })
+
+  // it('maxLength12333', () => {
+  //   let data = {
+  //     email: '123123123123'
+  //   }
+  //   let rules = [
+  //     {key: 'email', pattern: 'email', maxLength: 10}
+  //   ]
+  //   let errors = validate(data, rules)
+  //   expect(errors.email.maxLength).to.exist
+  // })
+
+
+  it('many keys', ()=> {
+    let data = {
+      email: '12345611231267'
+    }
+    let rules = [
+      {
+        key: 'email', required: true, minLength: 5, maxLength: 10,
+        hasNumber: true, hasLowerCaseAndUpperCase: true, hasDot: true, hasUnderScore: true,
+        hasFrank: true
+      }
+    ]
+    let fn = () => {
+      validate(data, rules)
+    }
+
+    expect(fn).to.throw()
+  })
+
+  it('自定义测试规则hasNumber', ()=>{
+    let data = {
+      email: 'abc'
+    }
+
+    validate.hasNumber = (value) => {
+      if (!/\d/.test(value)) {
+        return '必须含有数字'
+      }
+    }
+
+    let rules = [
+      {
+        key: 'email', required: true, minLength: 5, maxLength: 10,
+        hasNumber: true
+      }
+    ]
+    let errors
+    let fn = () => {
+      errors = validate(data, rules)
+    }
+
+    expect(fn).to.not.throw()
+    expect(errors.email.hasNumber).to.exist
+  })
+
+
+  let demoRule = {
+    key: 'xxx', required: true, minLength: 5, maxLength: 20,
+    hasNumber: true, hasLowerCaseAndUpperCase: true, hasDot: true, hasUnderScore: true,
+    hasFrank: true
+  }
+
+
 })
