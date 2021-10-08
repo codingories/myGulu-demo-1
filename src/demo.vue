a0073f8daaa3750e3b8fb0328251794452c7bf1a
朱骏杰 <zhujunjie@sundear.com>
2021-10-08 18:53:27 +0800

    117 创建uploader完成

--- src/demo.vue
+++ src/demo.vue
@@ -1,94 +1,28 @@
 <template>
   <div>
-    <div style="margin:20px;">
-      <g-table :columns="columns" :data-source="dataSource" bordered :selectedItems.sync="selected" :order-by.sync="orderBy"
-      @update:orderBy="x" :loading="loading" :height="400" expend-field="description"
-               checkable
-      >
-        <template slot-scope="xxx">
-          <button @click="edit(xxx.item)">编辑</button>
-          <button @click="view(xxx.item)">查看</button>
-        </template>
-      </g-table>
-    </div>
-<!--    <div style="margin:20px;">-->
-<!--      <g-table :columns="columns" :data-source="dataSource" bordered compact :striped="false"></g-table>-->
-<!--    </div>-->
-<!--    <div style="margin:20px;">-->
-<!--      <g-pager :total-page="10" :current-page.sync="currentPage" hide-if-one-page></g-pager>-->
-<!--    </div>-->
+    <g-uploader accept="image/*"  method="POST" action="http://127.0.0.1:3000/upload" name="file">
+      <button>上传</button>
+      <template slot="tips">
+        <div>只能上传 300 kb 以内的 png,jpeg 文件</div>
+      </template>
+    </g-uploader>
   </div>
 </template>
 <script>
-  import Gpager from './pager'
-  import GTable from './table'
-
+  import GUploader from './uploader'
   export default {
     name: '',
     components: {
-      'g-pager':Gpager,
-      'g-table': GTable
+      'GUploader':GUploader,
     },
 
     data(){
       return {
-        loading: false,
-        selectedItems: [],
-        currentPage: 1,
-        columns: [
-          {text:'姓名', field:'name', width: 100},
-          {text:'分数', field:'score' },
-        ],
-        orderBy: {
-          score: 'desc'
-        },
-        selected: [],
-        dataSource: [
-          {id: 1, name: '方方', score: 100, description: '11111'},
-          {id: 2, name: '圆圆', score: 99, description: '22222'},
-          {id: 3, name: '张三', score: 99},
-          {id: 4, name: '李四', score: 99},
-          {id: 5, name: '我', score: 99},
-          {id: 6, name: '的', score: 99},
-          {id: 7, name: '请求', score: 99},
-          {id: 8, name: '我', score: 99},
-          {id: 9, name: '的', score: 99},
-          {id: 10, name: '请求', score: 99},
-          {id: 11, name: '方方', score: 100},
-          {id: 12, name: '圆圆', score: 99},
-          {id: 13, name: '张三', score: 99},
-          {id: 14, name: '李四', score: 99},
-          {id: 15, name: '我', score: 99},
-          {id: 16, name: '的', score: 99},
-          {id: 17, name: '请求', score: 99},
-          {id: 18, name: '我', score: 99},
-          {id: 19, name: '的', score: 99},
-          {id: 20, name: '请求', score: 99},
-        ]
+
       }
     },
     methods: {
-      // x(object) {
-      //   console.log(object)
-      //   let {selected, item ,index} = object
-      //   console.log(selected)
-      //   console.log(item)
-      //   console.log(index)
-      // }
-      edit( item ) {
-        alert(`开始编辑${item.id}`)
-      },
-      view( item ) {
-        alert(`开始查看${item.id}`)
-      },
-      x() {
-        console.log('x')
-        this.loading = true
-        setTimeout(()=>{
-          this.dataSource = this.dataSource.sort((a, b) => a.score - b.score)
-          this.loading = false
-        },3000)
-      }
+
     }
 }
 </script>
---
+++ src/uploader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="gulu-uploader">
+    <div @click="onClickUpload">
+      <slot></slot>
+    </div>
+    <div ref="temp" style="width: 0;height: 0;  overflow:hidden;"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'GuluUploader',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    action: {
+      type: String,
+      required: true
+    },
+    method: {
+      type: String,
+      default: 'POST'
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    onClickUpload() {
+      let input = document.createElement('input')
+      input.type = 'file'
+      this.$refs.temp.appendChild(input)
+      input.addEventListener('change', () => {
+        let file = input.files[0]
+        input.remove()
+        console.log(file)
+        let formData = new FormData();
+        formData.append(this.name, file);
+        var xhr = new XMLHttpRequest()
+        xhr.open(this.method, this.action)
+        xhr.onload = function () {
+          console.log(xhr.response)
+        }
+        xhr.send(formData)
+      })
+      input.click()
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+.gulu-uploader {
+
+}
+</style>
